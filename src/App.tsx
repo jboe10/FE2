594e15f3fa2a35d1a7da53e193563b8cbb02e41f
jboe10 <jboesurf@gmail.com>
2023-09-09 12:39:47 -0700

    import fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,18 @@
 import { createContext, useState } from 'react';
 import './App.css';
 import EnhancedTable from './components/table/EnhancedTable';
-import EnhancedTableDropDownFilter from './components/DropDown';
-import EnhancedTableSearch from './components/table/TableSearch';
-import { createTheme, ThemeProvider } from '@mui/material';
 
 export const CreateOrderContext = createContext(null);
 function App() {
 	const [currentOrderContext, setCurrentOrderContext] = useState();
-	const theme = createTheme({
-		direction: 'rtl',
-	});
+
 	return (
 		<>
-			{/* <ThemeProvider theme={theme}> */}
 			<CreateOrderContext.Provider
 				value={{ currentOrderContext, setCurrentOrderContext }}
 			>
 				<EnhancedTable />
 			</CreateOrderContext.Provider>
-			{/* </ThemeProvider> */}
 		</>
 	);
 }
